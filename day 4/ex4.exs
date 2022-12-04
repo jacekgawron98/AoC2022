d41c6aad55d9a8eaec5915801f9b7d98f606050d
jacekgawron98 <gawron.jacek98@gmail.com>
2022-12-04 12:10:17 +0100

    day 4 cleaning

--- day 4/ex4.exs
+++ day 4/ex4.exs
@@ -1,39 +1,27 @@
 contained = File.stream!("ex4.txt")
-|>Enum.map(&String.trim/1)
 |>Enum.map(fn pair ->
-  String.split(pair, ",")
-  |>Enum.map(fn p ->
-    String.split(p, "-")
-    |>Enum.map(&Integer.parse/1)
-    |>Enum.map(&(elem(&1,0)))
-  end)
+  String.split(pair, [",","-"])
+  |>Enum.map(&Integer.parse/1)
+  |>Enum.map(&(elem(&1,0)))
 end)
-|>Enum.map(fn l ->
-  first = Enum.at(l,0)
-  second = Enum.at(l,1)
+|>Enum.map(fn [startA,endA,startB,endB] ->
   cond do
-    Enum.at(first,0) <= Enum.at(second,0) and Enum.at(first,1) >= Enum.at(second,1) -> 1
-    Enum.at(second,0) <= Enum.at(first,0) and Enum.at(second,1) >= Enum.at(first,1) -> 1
+    startA <= startB and endA >= endB -> 1
+    startB <= startA and endB >= endA -> 1
     true -> 0
   end
 end)
 |>Enum.sum()
 
 contained2 = File.stream!("ex4.txt")
-|>Enum.map(&String.trim/1)
 |>Enum.map(fn pair ->
-  String.split(pair, ",")
-  |>Enum.map(fn p ->
-    String.split(p, "-")
-    |>Enum.map(&Integer.parse/1)
-    |>Enum.map(&(elem(&1,0)))
-  end)
+  String.split(pair, [",","-"])
+  |>Enum.map(&Integer.parse/1)
+  |>Enum.map(&(elem(&1,0)))
 end)
-|>Enum.map(fn l ->
-  first = Enum.at(l,0)
-  second = Enum.at(l,1)
+|>Enum.map(fn [startA,endA,startB,endB] ->
   cond do
-    Enum.at(first,0) <= Enum.at(second,1) and Enum.at(first,1) >= Enum.at(second,0) -> 1
+    startA <= endB and endA >= startB -> 1
     true -> 0
   end
 end)
