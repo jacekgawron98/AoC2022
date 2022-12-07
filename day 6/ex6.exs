de01cbec8c5d9bb9ef234264655f938dc6a0e3c8
gawronj <jacek.gawron@comarch.com>
2022-12-07 13:19:48 +0100

    day 6 shorter answer

--- day 6/ex6.exs
+++ day 6/ex6.exs
@@ -2,25 +2,19 @@ defmodule Ex6 do
   def detect_distinct(len \\ 4) do
     elem(File.read("ex6.txt"),1)
     |>String.graphemes()
-    |>Enum.with_index()
-    |>Enum.reduce_while("", fn {s,index},acc ->
-      res = acc <> s
-      |>(&(
-        cond do
-          String.length(&1) > len -> String.slice(&1,1,String.length(&1))
-          true -> &1
-        end
-      )).()
-
-      map = res
-      |>String.graphemes()
-      |>MapSet.new()
-
-      cond do
-        MapSet.size(map) == len -> {:halt, index + 1}
-        true -> {:cont, res}
-      end
-    end)
+    |>(fn list -> Enum.with_index(list)
+      |>Enum.reduce_while("",
+      fn {s,index},acc ->
+        res = Enum.slice(list,index,len)
+        |>Enum.uniq()
+        |>(&(
+          cond do
+            length(&1) == len -> {:halt, index + len}
+            true -> {:cont, ""}
+          end
+        )).()
+      end)
+    end).()
   end
 end
 
